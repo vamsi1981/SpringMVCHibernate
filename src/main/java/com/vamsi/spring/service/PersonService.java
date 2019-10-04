905285aca25f7393c5faced676f9f9ce6f80506d
Vamsi Madiraju <vamsi.futurama@gmail.com>
2019-10-04 18:03:13 -0400

    first commit

---
+++ src/main/java/com/vamsi/spring/service/PersonService.java
@@ -0,0 +1,15 @@
+package com.vamsi.spring.service;
+
+import java.util.List;
+
+import com.vamsi.spring.model.Person;
+
+public interface PersonService {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+	
+}
